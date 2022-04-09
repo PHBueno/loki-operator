acbf1ce63f5ec34329cff6a6cb0f46c97bc9c20d
pedro <contato0113@gmail.com>
2022-04-09 17:26:53 -0300

    teste

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='loki-operator',
+    version='0.0.1',
+    author='Pedro Bueno',
+    description='An Operator to Loki',
+    packages=find_packages()
+)
